0a90891d88287f54468c87e5b22c1b7b1e5ceefa
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-28 14:45:55 +0600

    update

--- programRunningTime.js
+++ programRunningTime.js
@@ -1,9 +1,9 @@
 //performance.now()function
-
+//it's help to find out the programming running time
 const firstTime=performance.now()
 
-for(let i=1; i>50;i++){
-       print("time")
+for(let i=1; i<50;i++){
+       console.log("time")
 }
 
 const endTime=performance.now();
