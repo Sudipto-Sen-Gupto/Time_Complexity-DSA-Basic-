7436dd0a341d11a615a63b9ff7bb5676dab94cde
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-29 19:08:07 +0600

    convert array to set

---
+++ arrayToSet.js
@@ -0,0 +1,9 @@
+
+ const arr=['Physics','Chemistry','Biology','Physics']
+ arr.push('Math')
+ const set=new Set(arr)
+ set.add('Higher Math')
+ console.log(arr.length);
+ console.log(set.size);
+ console.log(arr);
+ console.log(set);
